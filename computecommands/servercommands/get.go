d058e7ec9f303981ad7fd7a5afe07b36bc867dd1
Jon Perritt <jrperritt@gmail.com>
2015-06-02 12:19:37 -0600

    servers tableGet

--- computecommands/servercommands/get.go
+++ computecommands/servercommands/get.go
@@ -29,7 +29,9 @@ func commandGet(c *cli.Context) {
 	util.CheckArgNum(c, 1)
 	serverID := c.Args()[0]
 	client := auth.NewClient("compute")
-	o, err := servers.Get(client, serverID).Extract()
+	s := servers.Get(client, serverID)
+	fmt.Printf("get server raw result: %+v\n", s)
+	o, err := s.Extract()
 	if err != nil {
 		fmt.Printf("Error retrieving server (%s): %s\n", serverID, err)
 		os.Exit(1)
@@ -40,9 +42,54 @@ func commandGet(c *cli.Context) {
 func tableGet(c *cli.Context, i interface{}) {
 	m := structs.Map(i)
 	t := tablewriter.NewWriter(c.App.Writer)
+	t.SetAlignment(tablewriter.ALIGN_LEFT)
 	t.SetHeader([]string{"property", "value"})
-	for k, v := range m {
-		t.Append([]string{k, fmt.Sprint(v)})
+	keys := []string{"ID", "Name", "Status", "Created", "Updated", "Image", "Flavor", "Public IPv4", "Public IPv6", "Private IPv4", "KeyName"}
+	for _, key := range keys {
+		tmp := ""
+		switch key {
+		case "Public IPv4":
+			tmp = fmt.Sprint(m["AccessIPv4"])
+		case "Public IPv6":
+			tmp = fmt.Sprint(m["AccessIPv6"])
+		case "Private IPv4":
+			i, ok := m["Addresses"].(map[string]interface{})
+			if !ok {
+				tmp = ""
+				break
+			}
+			j, ok := i["private"].([]interface{})
+			if !ok || len(j) == 0 {
+				tmp = ""
+				break
+			}
+			i, ok = j[0].(map[string]interface{})
+			if !ok {
+				tmp = ""
+				break
+			}
+			tmp = fmt.Sprint(i["addr"])
+		case "Image":
+			i, ok := m["Image"].(map[string]interface{})
+			if !ok {
+				tmp = ""
+				break
+			}
+			tmp = fmt.Sprint(i["id"])
+		case "Flavor":
+			i, ok := m["Flavor"].(map[string]interface{})
+			if !ok {
+				tmp = ""
+				break
+			}
+			tmp = fmt.Sprint(i["id"])
+		default:
+			tmp = fmt.Sprint(m[key])
+		}
+		if tmp == "<nil>" {
+			tmp = ""
+		}
+		t.Append([]string{key, tmp})
 	}
 	t.Render()
 }
