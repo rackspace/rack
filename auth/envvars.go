ae9375b2a4ce2e76cc66b63a6a37637fa2a843d6
Jon Perritt <jrperritt@gmail.com>
2015-06-09 16:19:59 -0600

    uppercase region

--- auth/envvars.go
+++ auth/envvars.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"os"
+	"strings"
 
 	"github.com/rackspace/gophercloud"
 	"github.com/rackspace/gophercloud/rackspace"
@@ -9,6 +10,6 @@ import (
 
 func envvars() (gophercloud.AuthOptions, string, error) {
 	ao, err := rackspace.AuthOptionsFromEnv()
-	region := os.Getenv("RS_REGION_NAME")
+	region := strings.ToUpper(os.Getenv("RS_REGION_NAME"))
 	return ao, region, err
 }
