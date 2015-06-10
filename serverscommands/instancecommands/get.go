e07f78a856ca28890cb2d69647bee779aaed4040
Kyle Kelley <rgbkrk@gmail.com>
2015-06-10 12:52:17 -0500

    Simplify map creation.

--- serverscommands/instancecommands/get.go
+++ serverscommands/instancecommands/get.go
@@ -46,51 +46,41 @@ func tableGet(c *cli.Context, i interface{}) {
 	t.SetAlignment(tablewriter.ALIGN_LEFT)
 	t.SetHeader([]string{"property", "value"})
 	keys := []string{"ID", "Name", "Status", "Created", "Updated", "Image", "Flavor", "Public IPv4", "Public IPv6", "Private IPv4", "KeyName"}
+
+	m["Public IPv4"] = m["AccessIPv4"]
+	m["Public IPv6"] = m["AccessIPv6"]
+
+	// Private IPv4 case
+	// Nested m["Addresses"]["private"][0]
+	addrs, ok := m["Addresses"].(map[string]interface{})
+	if ok {
+		ips, ok := addrs["private"].([]interface{})
+		if ok || len(ips) > 0 {
+			priv, ok := ips[0].(map[string]interface{})
+			if ok {
+				m["Private IPv4"] = priv["addr"]
+			}
+		}
+	}
+	if !ok { // if any were not ok, set the field to blank
+		m["Private IPv4"] = ""
+	}
+
+	flavor, ok := m["Flavor"].(map[string]interface{})
+	if ok {
+		m["Flavor"] = flavor["id"]
+	} else {
+		m["Flavor"] = ""
+	}
+	image, ok := m["Image"].(map[string]interface{})
+	if ok {
+		m["Image"] = image["id"]
+	} else {
+		m["Image"] = ""
+	}
+
 	for _, key := range keys {
-		tmp := ""
-		switch key {
-		case "Public IPv4":
-			tmp = fmt.Sprint(m["AccessIPv4"])
-		case "Public IPv6":
-			tmp = fmt.Sprint(m["AccessIPv6"])
-		case "Private IPv4":
-			i, ok := m["Addresses"].(map[string]interface{})
-			if !ok {
-				tmp = ""
-				break
-			}
-			j, ok := i["private"].([]interface{})
-			if !ok || len(j) == 0 {
-				tmp = ""
-				break
-			}
-			i, ok = j[0].(map[string]interface{})
-			if !ok {
-				tmp = ""
-				break
-			}
-			tmp = fmt.Sprint(i["addr"])
-		case "Image":
-			i, ok := m["Image"].(map[string]interface{})
-			if !ok {
-				tmp = ""
-				break
-			}
-			tmp = fmt.Sprint(i["id"])
-		case "Flavor":
-			i, ok := m["Flavor"].(map[string]interface{})
-			if !ok {
-				tmp = ""
-				break
-			}
-			tmp = fmt.Sprint(i["id"])
-		default:
-			tmp = fmt.Sprint(m[key])
-		}
-		if tmp == "<nil>" {
-			tmp = ""
-		}
-		t.Append([]string{key, tmp})
+		t.Append([]string{key, fmt.Sprint(m[key])})
 	}
 	t.Render()
 }
