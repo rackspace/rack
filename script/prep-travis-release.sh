ca3bdff080e4e6b1544f94077d4f5ef86b4ecbb4
Kyle Kelley <rgbkrk@gmail.com>
2015-06-11 09:15:37 -0500

    Clean up build, spit out binary

--- script/prep-travis-release.sh
+++ script/prep-travis-release.sh
@@ -2,34 +2,25 @@
 
 set -eo
 
-mkdir -p build
-mkdir -p build/pr/
+os=$GIMME_OS
+arch=$GIMME_ARCH
 
+echo "Building for ${os}-${arch}"
+
+mkdir -p build
 # Provide a commit hash version
 COMMIT=`git rev-parse HEAD 2> /dev/null`
 mkdir -p "build/commits/${COMMIT}"
 
-eval "$(gimme 1.4)"
-
-os=$GIMME_OS
-arch=$GIMME_ARCH
-
-echo "Pre-flight diagnostics"
-ls
-pwd
-
-echo "Building for ${os}-${arch}"
-
 BASENAME="rack-${os}-${arch}"
 RACKBUILD="build/${BASENAME}"
+
+go build -a -o $RACKBUILD
 
 SUFFIX=""
 if [ "$os" == "windows" ]; then
   SUFFIX=".exe"
 fi
-
-# Create build/rack, making sure to start over without build artifacts
-go build -a -o $RACKBUILD
 
 # See http://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
 # for details about default Travis Environment Variables and their values
