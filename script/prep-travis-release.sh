a067d4b7e4804aa1fb118a93e4dfdff4a2dd3f16
Kyle Kelley <rgbkrk@gmail.com>
2015-06-11 07:44:09 -0500

    Create a build matrix.

--- script/prep-travis-release.sh
+++ script/prep-travis-release.sh
@@ -9,59 +9,46 @@ mkdir -p build/pr/
 COMMIT=`git rev-parse HEAD 2> /dev/null`
 mkdir -p "build/commits/${COMMIT}"
 
-echo '
-  darwin   amd64    OS X
-  freebsd  386      FreeBSD 32-bit
-  freebsd  amd64    FreeBSD 64-bit
-  linux    386      Linux 32-bit
-  linux    amd64    Linux 64-bit
-  windows  386      Windows 32-bit
-  windows  amd64    Windows 64-bit
-' | {
-  while read os arch label; do
-    [ -n "$os" ] || continue
-    export GIMME_OS="$os"
-    export GIMME_ARCH="$arch"
-    eval "$(gimme 1.4)"
+eval "$(gimme 1.4)"
 
-    echo "Building for $GIMME_OS-$GIMME_ARCH"
+os=$GIMME_OS
+arch=$GIMME_ARCH
 
-    BASENAME="rack-${os}-${arch}"
-    RACKBUILD="build/${BASENAME}"
+echo "Building for ${os}-${arch}"
 
-    SUFFIX=""
-    if [ "$os" == "windows" ]; then
-      SUFFIX=".exe"
-    fi
+BASENAME="rack-${os}-${arch}"
+RACKBUILD="build/${BASENAME}"
 
-    # Create build/rack
-    go build -o $RACKBUILD
+SUFFIX=""
+if [ "$os" == "windows" ]; then
+  SUFFIX=".exe"
+fi
 
-    # See http://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
-    # for details about default Travis Environment Variables and their values
-    if [ -z "$TRAVIS_BRANCH" ]; then
-      BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-      cp $RACKBUILD build/${BASENAME}-${BRANCH}${SUFFIX}
-    else
-      # Ship a PR binary
-      if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-        cp $RACKBUILD build/pr/${BASENAME}-pr${TRAVIS_PULL_REQUEST}${SUFFIX}
-      fi
+# Create build/rack, making sure to start over without build artifacts
+go build -a -o $RACKBUILD
 
-      BRANCH=$TRAVIS_BRANCH
-    fi
+# See http://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
+# for details about default Travis Environment Variables and their values
+if [ -z "$TRAVIS_BRANCH" ]; then
+  BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+  cp $RACKBUILD build/${BASENAME}-${BRANCH}${SUFFIX}
+else
+  # Ship a PR binary
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    cp $RACKBUILD build/pr/${BASENAME}-pr${TRAVIS_PULL_REQUEST}${SUFFIX}
+  fi
 
-    cp $RACKBUILD build/commits/${COMMIT}/${BASENAME}-${COMMIT}${SUFFIX}
+  BRANCH=$TRAVIS_BRANCH
+fi
 
-    if [ "$BRANCH" != "master" ]; then
-      # Ship /rack-branchname
-      cp $RACKBUILD build/${BASENAME}-${BRANCH}${SUFFIX}
-      # Remove our artifact
-      rm $RACKBUILD
-    elif [ "$os" == "windows" ]; then
-      cp $RACKBUILD $RACKBUILD${SUFFIX}
-      rm $RACKBUILD
-    fi
+cp $RACKBUILD build/commits/${COMMIT}/${BASENAME}-${COMMIT}${SUFFIX}
 
-  done
-}
+if [ "$BRANCH" != "master" ]; then
+  # Ship /rack-branchname
+  cp $RACKBUILD build/${BASENAME}-${BRANCH}${SUFFIX}
+  # Remove our artifact
+  rm $RACKBUILD
+elif [ "$os" == "windows" ]; then
+  cp $RACKBUILD $RACKBUILD${SUFFIX}
+  rm $RACKBUILD
+fi
