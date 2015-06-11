508a4db4005346d1a56a4959bb41b896b920265e
Jon Perritt <jrperritt@gmail.com>
2015-06-10 20:48:11 -0600

    csv test case

--- output/table.go
+++ output/table.go
@@ -1,6 +1,7 @@
 package output
 
 import (
+	"encoding/csv"
 	"fmt"
 	"strings"
 	"text/tabwriter"
@@ -27,12 +28,23 @@ func ListTable(c *cli.Context, f *func() []map[string]interface{}, keys []string
 
 // MetadataTable writes standardized metadata out
 func MetadataTable(c *cli.Context, f *func() map[string]interface{}, keys []string) {
-	w := tabwriter.NewWriter(c.App.Writer, 0, 8, 0, '\t', 0)
-	m := (*f)()
-	fmt.Fprintln(w, "PROPERTY\tVALUE")
-	for _, key := range keys {
-		val := fmt.Sprint(m[key])
-		fmt.Fprintf(w, "%s\t%s\n", key, strings.Replace(val, "\n", "\n\t", -1))
+	if c.IsSet("csv") {
+		w := csv.NewWriter(c.App.Writer)
+		m := (*f)()
+		w.Write([]string{"PROPERTY", "VALUE"})
+		for _, key := range keys {
+			val := fmt.Sprint(m[key])
+			w.Write([]string{key, val})
+		}
+		w.Flush()
+	} else {
+		w := tabwriter.NewWriter(c.App.Writer, 0, 8, 0, '\t', 0)
+		m := (*f)()
+		fmt.Fprintln(w, "PROPERTY\tVALUE")
+		for _, key := range keys {
+			val := fmt.Sprint(m[key])
+			fmt.Fprintf(w, "%s\t%s\n", key, strings.Replace(val, "\n", "\n\t", -1))
+		}
+		w.Flush()
 	}
-	w.Flush()
 }
