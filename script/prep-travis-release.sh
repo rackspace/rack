9f6dd2271fc009038733beba4ceb55f06446fd84
Kyle Kelley <rgbkrk@gmail.com>
2015-06-15 12:57:06 -0500

    Be friendlier to uname on nix, rack.exe on windows
    * Partially addresses #57 (uname for some)
    * Default download for Windows is now `rack.exe` (per #53)
    
    Note that the uname approach from #57 will not work for any ARM based installations.
    
    /cc @carolynvs @everett-toews

--- script/prep-travis-release.sh
+++ script/prep-travis-release.sh
@@ -18,6 +18,11 @@ IFS=$'\n\t'
 ################################################################################
 set +u
 
+if [[ -z "$GIMME_OS" && -z "$GIMME_ARCH" ]]; then
+  echo "GIMME_OS and GIMME_ARCH must be defined"
+  exit 2
+fi
+
 os=$GIMME_OS
 arch=$GIMME_ARCH
 # See http://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
@@ -37,12 +42,40 @@ fi
 set -u
 ################################################################################
 
-SUFFIX=""
-if [ "$arch" == "arm" ]; then
-  arch="armv${GOARM}"
-fi
-if [ "$os" == "windows" ]; then
-  SUFFIX="${SUFFIX}.exe"
+case $os in
+  windows)
+    os="Windows"
+    ;;
+  linux)
+    os="Linux"
+    ;;
+  darwin)
+    os="Darwin"
+    ;;
+  freebsd)
+    os="FreeBSD"
+    ;;
+  *)
+    echo "Unknown OS ${os}. Assuming it's a valid OS for gimme/go and charging ahead."
+esac
+
+case $arch in
+  arm)
+    # Note that we can never be 1:1 between `uname -m` and arm versions
+    arch="armv${GOARM}"
+    SUFFIX="-${os}-${arch}"
+    ;;
+esac
+
+if [[ "$os" == "Windows" && "$arch" == "amd64" ]]; then
+  SUFFIX=".exe"
+elif [[ "$os" == "Windows" && "$arch" != "amd64" ]]; then
+  SUFFIX="-${arch}.exe"
+elif [ "$arch" == "amd64" ]; then
+  # Assume 64 bit gets to be as is
+  SUFFIX="-${os}"
+else
+  SUFFIX="-${os}-${arch}"
 fi
 
 echo "Building for ${os}-${arch}"
@@ -52,8 +85,8 @@ mkdir -p build
 COMMIT=`git rev-parse HEAD 2> /dev/null`
 mkdir -p "build/commits/${COMMIT}"
 
-BASENAME="rack-${os}-${arch}"
-RACKBUILD="build/${BASENAME}"
+BASENAME="rack"
+RACKBUILD="build/${BASENAME}${SUFFIX}"
 
 go build -o $RACKBUILD
 
@@ -63,8 +96,5 @@ if [ "$BRANCH" != "master" ]; then
   # Ship /rack-branchname
   cp $RACKBUILD build/${BASENAME}-${BRANCH}${SUFFIX}
   # Remove our artifact
-  rm $RACKBUILD
-elif [ "$os" == "windows" ]; then
-  cp $RACKBUILD $RACKBUILD${SUFFIX}
   rm $RACKBUILD
 fi
