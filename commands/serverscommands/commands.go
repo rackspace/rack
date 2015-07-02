3dd2a70fd8a6e08487404a2884ef0cc100b63768
Kyle Kelley <rgbkrk@gmail.com>
2015-07-02 11:12:35 -0500

    Flesh out the servers section.

--- commands/serverscommands/commands.go
+++ commands/serverscommands/commands.go
@@ -1,11 +1,11 @@
 package serverscommands
 
 import (
-	"github.com/jrperritt/rack/internal/github.com/codegangsta/cli"
 	"github.com/jrperritt/rack/commands/serverscommands/flavorcommands"
 	"github.com/jrperritt/rack/commands/serverscommands/imagecommands"
 	"github.com/jrperritt/rack/commands/serverscommands/instancecommands"
 	"github.com/jrperritt/rack/commands/serverscommands/keypaircommands"
+	"github.com/jrperritt/rack/internal/github.com/codegangsta/cli"
 )
 
 // Get returns all the commands allowed for a `servers` request.
@@ -13,22 +13,22 @@ func Get() []cli.Command {
 	return []cli.Command{
 		{
 			Name:        "instance",
-			Usage:       "Used for Server Instance operations",
+			Usage:       "Servers.",
 			Subcommands: instancecommands.Get(),
 		},
 		{
 			Name:        "image",
-			Usage:       "Used for Server Image operations",
+			Usage:       "Base operating system layout for a server.",
 			Subcommands: imagecommands.Get(),
 		},
 		{
 			Name:        "flavor",
-			Usage:       "Used for Server Flavor operations",
+			Usage:       "Resource allocations for servers.",
 			Subcommands: flavorcommands.Get(),
 		},
 		{
 			Name:        "keypair",
-			Usage:       "Used for Server Keypair operations",
+			Usage:       "SSH keypairs for accessing servers.",
 			Subcommands: keypaircommands.Get(),
 		},
 	}
