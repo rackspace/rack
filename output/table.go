c3b5f554a598d1512712f5c76675073958182216
Kyle Kelley <rgbkrk@gmail.com>
2015-07-02 11:18:39 -0500

    Goodbye PROPVAL, hello comments.

--- output/table.go
+++ output/table.go
@@ -7,6 +7,7 @@ import (
 	"text/tabwriter"
 )
 
+// ListTable writes a table composed of keys as the header with values from many
 func ListTable(writer io.Writer, many []map[string]interface{}, keys []string) {
 	w := tabwriter.NewWriter(writer, 0, 8, 1, '\t', 0)
 	// Write the header
@@ -21,9 +22,10 @@ func ListTable(writer io.Writer, many []map[string]interface{}, keys []string) {
 	w.Flush()
 }
 
+// MetadataTable writes a table to the writer composed of keys on the left and
+// the associated metadata on the right column from m
 func MetadataTable(writer io.Writer, m map[string]interface{}, keys []string) {
 	w := tabwriter.NewWriter(writer, 0, 8, 0, '\t', 0)
-	fmt.Fprintln(w, "PROPERTY\tVALUE")
 	for _, key := range keys {
 		val := fmt.Sprint(m[key])
 		fmt.Fprintf(w, "%s\t%s\n", key, strings.Replace(val, "\n", "\n\t", -1))
