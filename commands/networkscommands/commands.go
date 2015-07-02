04808c1caf8022755fd05168b0663753c6041ad0
Kyle Kelley <rgbkrk@gmail.com>
2015-07-02 11:11:03 -0500

    Flesh out the network section.

--- commands/networkscommands/commands.go
+++ commands/networkscommands/commands.go
@@ -1,12 +1,12 @@
 package networkscommands
 
 import (
-	"github.com/jrperritt/rack/internal/github.com/codegangsta/cli"
 	"github.com/jrperritt/rack/commands/networkscommands/networkcommands"
 	"github.com/jrperritt/rack/commands/networkscommands/portcommands"
 	"github.com/jrperritt/rack/commands/networkscommands/securitygroupcommands"
 	"github.com/jrperritt/rack/commands/networkscommands/securitygrouprulecommands"
 	"github.com/jrperritt/rack/commands/networkscommands/subnetcommands"
+	"github.com/jrperritt/rack/internal/github.com/codegangsta/cli"
 )
 
 // Get returns all the commands allowed for a `networks` request.
@@ -14,27 +14,27 @@ func Get() []cli.Command {
 	return []cli.Command{
 		{
 			Name:        "network",
-			Usage:       "Used for Cloud Networks network operations",
+			Usage:       "Set up new networks to use by servers.",
 			Subcommands: networkcommands.Get(),
 		},
 		{
 			Name:        "subnet",
-			Usage:       "Used for Cloud Networks subnet operations",
+			Usage:       "Allocate IP address blocks, gateways, DNS servers, and host routes to networks.",
 			Subcommands: subnetcommands.Get(),
 		},
 		{
 			Name:        "port",
-			Usage:       "Used for Cloud Networks port operations",
+			Usage:       "Virtual switch ports on logical network switches.",
 			Subcommands: portcommands.Get(),
 		},
 		{
 			Name:        "security-group",
-			Usage:       "Used for Cloud Networks security group operations",
+			Usage:       "Collections of rules for network traffic.",
 			Subcommands: securitygroupcommands.Get(),
 		},
 		{
 			Name:        "security-group-rule",
-			Usage:       "Used for Cloud Networks security group rule operations",
+			Usage:       "Rules for network traffic.",
 			Subcommands: securitygrouprulecommands.Get(),
 		},
 	}
