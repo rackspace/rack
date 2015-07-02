10cf4983dffbc4bd1e45e965ecc448a9cf51e6d8
Jon Perritt <jrperritt@gmail.com>
2015-07-02 13:27:49 -0600

    consolidate global flags into one function

--- util/globalflags.go
+++ util/globalflags.go
@@ -2,20 +2,9 @@ package util
 
 import "github.com/jrperritt/rack/internal/github.com/codegangsta/cli"
 
-// OutputFlags are global flags (i.e. flags that all commands can use) that let
-// users specify the format of the output from a command.
-func OutputFlags() []cli.Flag {
-	return []cli.Flag{
-		cli.StringFlag{
-			Name:  "output",
-			Usage: "Format in which to return output. Options: json, csv, table. Default is 'table'.",
-		},
-	}
-}
-
-// AuthFlags are global flags (i.e. flags that all commands can use) that let
-// users specify authentication parameters.
-func AuthFlags() []cli.Flag {
+// GlobalFlags returns the flags that can be used after `rack` in a command, such as
+// output flags and authentication flags.
+func GlobalFlags() []cli.Flag {
 	return []cli.Flag{
 		cli.StringFlag{
 			Name:  "username",
@@ -37,17 +26,13 @@ func AuthFlags() []cli.Flag {
 			Name:  "profile",
 			Usage: "The config file profile to use for authentication.",
 		},
+		cli.StringFlag{
+			Name:  "output",
+			Usage: "Format in which to return output. Options: json, csv, table. Default is 'table'.",
+		},
 		cli.BoolFlag{
 			Name:  "no-cache",
 			Usage: "Don't get or set authentication credentials in the rack cache.",
 		},
 	}
-}
-
-// GlobalFlags returns the flags that can be used after `rack` in a command, such as
-// output flags and authentication flags.
-func GlobalFlags() []cli.Flag {
-	gFlags := OutputFlags()
-	gFlags = append(gFlags, AuthFlags()...)
-	return gFlags
 }
