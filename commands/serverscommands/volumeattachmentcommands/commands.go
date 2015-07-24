6197f2bf64bdf54fc93d39fe4cb565238abae094
Jon Perritt <jrperritt@gmail.com>
2015-07-23 19:44:48 -0600

    typo fix in comment

--- commands/serverscommands/volumeattachmentcommands/commands.go
+++ commands/serverscommands/volumeattachmentcommands/commands.go
@@ -5,7 +5,7 @@ import "github.com/jrperritt/rack/internal/github.com/codegangsta/cli"
 var commandPrefix = "servers volume-attachment"
 var serviceClientType = "compute"
 
-// Get returns all the commands allowed for a `servers volumeattachment` request.
+// Get returns all the commands allowed for a `servers volume-attachment` request.
 func Get() []cli.Command {
 	return []cli.Command{
 		list,
