f497f17520ab0ff4300679e4f079436132ba68b5
jrperritt <jrperritt@gmail.com>
2015-07-27 22:26:33 -0600

    fix for bash completion: take 2

--- setup/commandcompletion_bash.sh
+++ setup/commandcompletion_bash.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
 
 _cli_bash_autocomplete() {
-  local cur prev opts base
+  local cur prev opts
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
-  if [[ "${#COMP_WORDS[@]}" > 4 ]] && [[ ${cur} != -* ]] && [[ -n "${cur}" ]]; then
+
+  if [[ ${prev} != -* ]]; then
+    opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
     return 0
   fi
+
+  if [[ "${#COMP_WORDS[@]}" > 4 ]] && [[ ${cur} != -* ]]; then
+    return 0
+  fi
+
   opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
   COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
   return 0
--- setup/setup.go
+++ setup/setup.go
@@ -17,16 +17,24 @@ var rackBashAutocomplete = `
 #! /bin/bash
 
 _cli_bash_autocomplete() {
-	local cur prev opts base
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	if [[ "${#COMP_WORDS[@]}" > 4 ]] && [[ ${cur} != -* ]] && [[ -n "${cur}" ]]; then
-  	return 0
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+  if [[ ${prev} != -* ]]; then
+    opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
   fi
-	opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
-	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-	return 0
+
+  if [[ "${#COMP_WORDS[@]}" > 4 ]] && [[ ${cur} != -* ]]; then
+    return 0
+  fi
+
+  opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+  return 0
 }
 
 complete -F _cli_bash_autocomplete rack
