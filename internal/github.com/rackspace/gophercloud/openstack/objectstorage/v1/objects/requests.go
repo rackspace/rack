f679abdfec517729850002474aacc9e800e52249
jrperritt <jrperritt@gmail.com>
2015-07-31 19:17:25 -0600

    vendor gophercloud 'object create' bug fix

--- internal/github.com/rackspace/gophercloud/openstack/objectstorage/v1/objects/requests.go
+++ internal/github.com/rackspace/gophercloud/openstack/objectstorage/v1/objects/requests.go
@@ -237,9 +237,10 @@ func Create(c *gophercloud.ServiceClient, containerName, objectName string, cont
 	if resp != nil {
 		res.Header = resp.Header
 		if resp.Header.Get("ETag") == fmt.Sprintf("%x", localChecksum) {
-			res.Err = nil
+			res.Err = err
 			return res
 		}
+		res.Err = fmt.Errorf("Local checksum does not match API ETag header")
 	}
 
 	return res
