991f7584840250337faf23af2e0931a7cf17d496
Kyle Kelley <rgbkrk@gmail.com>
2015-09-16 15:18:19 -0500

    Finish appeasing shellcheck.

--- script/lib.sh
+++ script/lib.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 get_branch() {
   # See http://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
   # for details about default Travis Environment Variables and their values
@@ -6,18 +8,25 @@ get_branch() {
   else
     BRANCH=$TRAVIS_BRANCH
   fi
+  export BRANCH
 }
 
 get_commit() {
   COMMIT=$(git rev-parse --verify HEAD)
-  return $?
+  RETURN_CODE=$?
+  export COMMIT
+  return $RETURN_CODE
 }
 
 get_version() {
   if [ -z "$TRAVIS_TAG" ]; then
       # Version will be the most recent tag, appended with -dev (e.g. 1.0.0-dev)
       OLD_TAG=$(git describe --tags 2> /dev/null)
+
       VERSION="${OLD_TAG}-dev"
+
+      # If an old tag wasn't found, set it to dev.
+      # Note: the egg came before the chicken
       if [ "$OLD_TAG" == "" ]; then
           VERSION="dev"
       fi
@@ -25,6 +34,7 @@ get_version() {
       # We have ourselves a *real* release
       VERSION=$TRAVIS_TAG
   fi
+  export VERSION
 
   return 0
 }
