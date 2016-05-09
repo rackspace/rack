dc0d06b00107f8d2360da78ae207151724b818df
jrperritt <jrperritt@gmail.com>
2016-05-09 16:32:09 -0500

    comment fix

--- commands/serverscommands/imagecommands/commands.go
+++ commands/serverscommands/imagecommands/commands.go
@@ -5,7 +5,7 @@ import "github.com/rackspace/rack/internal/github.com/codegangsta/cli"
 var commandPrefix = "servers image"
 var serviceClientType = "compute"
 
-// Get returns all the commands allowed for a `servers images` request.
+// Get returns all the commands allowed for a `servers image` request.
 func Get() []cli.Command {
 	return []cli.Command{
 		list,
